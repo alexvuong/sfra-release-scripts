e3faf37907796bc2fa121f6b2ee0148b27d882a8
Alex Vuong <alex.vuong@salesforce.com>
2024-07-09 14:22:53 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,119 @@
+# Introduction
+This package includes scripts that will automate the SFRA and its plugins release process. This includes
+1. Prep github PRs for all of the plugins and base cartridge
+
+# Prerequisites
+
+You need to install [gh](https://github.com/cli/cli) cli to allow the script to create PRs on your behalf.
+After you install, please login into github with your ssh key
+```
+gh auth login
+```
+
+# How to use
+
+```
+node ./prep-PR.js.js <VERSION> <MAIN_BRANCH> <action> [<package1> <package2> ... <packageN>]
+```
+- VERSION: the version you want to upgrade to
+- MAIN_BRAIN: the base branch to branch of for release
+- action
+  - createPRs: to prepare the data (update version, do any necessary file changes in all the cartridges) and create PRs for the release branch
+  - createGitTag: After all of the PRs are approved and merged, you want to make sure you create and push new tags to all of the cartridges.
+- [<package1> <package2> ... <packageN>]: list of packages you want to prepare for release
+
+### Examples
+Example for PR creation with specific packages:
+
+```
+>node prep-PR.js v1.0.0 main createPR lib_productlist plugin-applepay
+
+```
+
+Example for PR creation with default packages.
+```
+>node prep-PR.js v1.0.0 main createPR
+
+// default packages
+const defaultPackages = [
+    'lib_productlist',
+    'plugin-applepay',
+    'plugin_cartridge_merge',
+    'plugin_datadownload',
+    'plugin_giftregistry',
+    'plugin_instorepickup',
+    'plugin_ordermanagement',
+    'plugin_productcompare',
+    'plugin_sitemap',
+    // 'plugin_slas',
+    'plugin_wishlists',
+    'storefrontdata',
+    'storefront-reference-architecture'
+]
+```
+
+Example for Git tag creation with specific packages.
+```
+>node prep-PR.js v1.0.0 main createGitTag lib_productlist plugin-applepay
+```
+
+### Steps that the script does
+```
+********************CreatePRs action ******
+Follow these steps in each of the repositories listed under Repositories to Release, above.
+
+1. Set bash variables for easy copy/pasting of these steps!
+2. VERSION="v0.0.0" # Set to the new release version. Include the v!
+    MAIN_BRANCH="master" # Use "integration" for SFRA base and "develop" for plugin_slas
+3. Check out the latest code.
+    1. Double check that your working tree is clean.
+    2. git status --short
+        # Stash changes, if necessary
+        git stash push --include-untracked -m "WIP before $VERSION release"
+    3. Download the latest from the main branch.
+    4. git switch $MAIN_BRANCH
+        git pull
+    5. Double check that everything is clean!
+    6. git status --short
+4. Create a release branch.
+5. git switch -c release/$VERSION
+6. Ensure dependencies are up to date.
+7. npm install
+    # Commit changes, if any.
+    git add package-lock.json
+    git commit -m "chore: update dependencies"
+8. Update references to the current version in each repository.
+    1. Update repository-specific files. See Repository-Specific Changes below for details.
+    2. Update cartridge versions in cartridges/<cartridge_name>/cartridge/<cartridge_name>.properties file (Create the file if one does not exist)
+        1. Set demandware.cartridges.<cartridge_name>.version=<new_release_version>
+    3. Update the package.json and package-lock.json.
+        ⚠️ DO NOT use npm version, it will break our release tags.
+        ⚠️ DO NOT use npm run release in the SFRA base repo. It uses npm version under the hood.
+    4. # Update package.json manually - don't include the "v" in $VERSION!
+        npm install # Copy change to package-lock.json
+    5. Commit changes.
+    6. # git add <repo-specific files>
+        git add package.json package-lock.json
+        git commit -m "chore: release $VERSION"
+    7. Add the changelog!    
+9. Publish changes.
+10. git push -u origin release/$VERSION
+11. Create a GitHub PR and merge it down. Optionally, use the GitHub CLI (install w/ brew install gh).
+12. # Use the web UI or the CLI
+    gh pr create --fill
+    gh pr view --web
+    1. For SFRA Base, make sure the PR is for the integration branch and not the master branch
+    
+    
+***************************CreateGitTag action***********************
+13. Create and publish a git tag after merging.
+14. git switch $MAIN_BRANCH
+    git pull
+    git tag $VERSION
+    git push origin refs/tags/$VERSION
+15. Create a GitHub release.
+16. # Use the web UI or the CLI
+    gh release view --web
+
+```
+
---
+++ prep-PRs.js
@@ -0,0 +1,394 @@
+#!/usr/bin/env node
+const {execSync} = require('child_process')
+const readline = require('readline')
+const path = require('path')
+const fs = require('fs')
+const os = require('os')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const defaultPackages = [
+    'lib_productlist',
+    'plugin-applepay',
+    'plugin_cartridge_merge',
+    'plugin_datadownload',
+    'plugin_giftregistry',
+    'plugin_instorepickup',
+    'plugin_ordermanagement',
+    'plugin_productcompare',
+    'plugin_sitemap',
+    'plugin_slas',
+    'plugin_wishlists',
+    'storefrontdata',
+    'storefront-reference-architecture'
+]
+// eslint-disable-next-line
+function execCommand(command, options = {}) {
+    try {
+        return execSync(command, {stdio: 'inherit', ...options})
+    } catch (error) {
+        console.log('error', error)
+        console.error(`Error executing command: ${command}`)
+        process.exit(1)
+    }
+}
+
+function promptUser(message) {
+    return new Promise((resolve) => {
+        rl.question(message, (answer) => {
+            resolve(answer.toLowerCase())
+        })
+    })
+}
+
+function updatePropertiesFile(packageName, VERSION) {
+    console.log('\x1b[33m>Updating properties file\x1b[0m')
+    const versionWithoutV = VERSION.replace(/^v/, '')
+
+    if (packageName === 'storefront-reference-architecture') {
+        console.log(
+            '\x1b[33m> Updating version.properties file for storefront-reference-architecture\n\x1b[0m'
+        )
+        const propertiesFilePath = path.resolve(
+            'cartridges/app_storefront_base/cartridge/templates/resources/version.properties'
+        )
+        if (fs.existsSync(propertiesFilePath)) {
+            let content = fs.readFileSync(propertiesFilePath, 'utf8')
+            const regex = /global\.version\.number=\S+/
+            const newLine = `global.version.number=${versionWithoutV}`
+
+            if (content.match(regex)) {
+                content = content.replace(regex, newLine)
+                fs.writeFileSync(propertiesFilePath, content, 'utf8')
+                console.log(`Updated ${propertiesFilePath} with new version ${versionWithoutV}`)
+            }
+        } else {
+            console.warn(`version.properties file ${propertiesFilePath} does not exist.`)
+        }
+        // Update app_storefront_base.properties file
+        console.log(
+            '\x1b[33m> Updating app_storefront_base.properties file for storefront-reference-architecture\n'
+        )
+        const appStorefrontBasePropertiesFilePath = path.resolve(
+            'cartridges/app_storefront_base/cartridge/app_storefront_base.properties'
+        )
+        if (fs.existsSync(appStorefrontBasePropertiesFilePath)) {
+            let content = fs.readFileSync(appStorefrontBasePropertiesFilePath, 'utf8')
+            const regex = /demandware\.cartridges\.app_storefront_base\.version=\S+/
+            const newLine = `demandware.cartridges.app_storefront_base.version=${versionWithoutV}`
+
+            if (content.match(regex)) {
+                content = content.replace(regex, newLine)
+                fs.writeFileSync(appStorefrontBasePropertiesFilePath, content, 'utf8')
+                console.log(
+                    `Updated ${appStorefrontBasePropertiesFilePath} with new version ${versionWithoutV}`
+                )
+            }
+        } else {
+            console.warn(
+                `app_storefront_base.properties file ${appStorefrontBasePropertiesFilePath} does not exist.`
+            )
+        }
+    } else {
+        const propertiesFilePath = path.resolve(
+            `cartridges/${packageName}/cartridge/${packageName}.properties`
+        )
+
+        if (fs.existsSync(propertiesFilePath)) {
+            const versionWithoutV = VERSION.replace(/^v/, '')
+            let content = fs.readFileSync(propertiesFilePath, 'utf8')
+            const regex = new RegExp(`demandware\\.cartridges\\.${packageName}\\.version=\\S+`)
+            const newLine = `demandware.cartridges.${packageName}.version=${versionWithoutV}`
+
+            if (content.match(regex)) {
+                content = content.replace(regex, newLine)
+                fs.writeFileSync(propertiesFilePath, content, 'utf8')
+                console.log(`Updated ${propertiesFilePath} with new version ${VERSION}`)
+            }
+        } else {
+            console.warn(
+                `\x1b[33m>>Properties file ${propertiesFilePath} does not exist. Skip this step for ${packageName}\x1b[0m`
+            )
+        }
+    }
+}
+
+function updatePackageJsonVersion(packageName, VERSION) {
+    const versionWithoutV = VERSION.replace(/^v/, '')
+    console.log('\x1b[33m>Updating package.json file\x1b[0m')
+    const packageJsonPath = path.resolve('package.json')
+    if (fs.existsSync(packageJsonPath)) {
+        const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'))
+        packageJson.version = versionWithoutV
+        fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf8')
+        console.log(
+            `\x1b[33m>>Updated ${packageJsonPath} with new version ${versionWithoutV}\x1b[0m`
+        )
+        // Run npm install to update package-lock.json
+        execCommand('npm install')
+    } else {
+        console.warn(`package.json file ${packageJsonPath} does not exist.`)
+    }
+}
+
+async function createPR(packageName, branchToUse, VERSION) {
+    console.log('\x1b[33m>Creating PR\x1b[0m')
+    // Check if PR already exists for release/${VERSION}
+    console.log(`\x1b[33m>>Checking open PRs: gh pr list --state open --base ${branchToUse}\x1b[0m`)
+    const prListOutput = execSync(`gh pr list --state open --base ${branchToUse}`).toString()
+    if (prListOutput.includes(`release/${VERSION}`)) {
+        console.log(`PR for release/${VERSION} already exists. Skipping PR creation.`)
+    } else {
+        console.log(`\x1b[33m>>Creating PRs: gh pr list --state open --base ${branchToUse}\x1b[0m`)
+        const createPrOutput = execSync(`gh pr create --fill --base ${branchToUse}`).toString()
+        const prNumberMatch = createPrOutput.match(/(\d+)/) // Extract PR number from output
+        const prNumber = prNumberMatch ? prNumberMatch[0] : null
+
+        if (prNumber) {
+            const prURL = `https://github.com/SalesforceCommerceCloud/${packageName}/pull/${prNumber}`
+            return prURL
+        } else {
+            console.error('Failed to create PR. No PR number found.')
+            return null
+        }
+    }
+}
+
+async function checkForChanges(promptUserForCommit = true, commitMsg = 'commit changed files') {
+    try {
+        const status = execSync('git status --porcelain').toString()
+        if (status) {
+            console.log('You have uncommitted changes:')
+            console.log(status)
+            const changedFiles = status
+                .split('\n')
+                .filter((line) => line.trim() !== '')
+                .map((line) => line.slice(3)) // Remove the status code at the beginning of each line
+
+            if (promptUserForCommit) {
+                const answer = await promptUser('Do you want to commit these changes? (y/n): ')
+                if (answer !== 'y') {
+                    console.log('Aborting.....Please clean up your working tree.')
+                    process.exit(0)
+                }
+
+                const commitMessage = await promptUser('Enter commit message: ')
+                // eslint-disable-next-line no-restricted-syntax
+                for (const file of changedFiles) {
+                    execCommand(`git add ${file}`)
+                }
+                execCommand(`git commit -m "${commitMessage}"`)
+            } else {
+                console.log('changedFiles', changedFiles)
+                for (const file of changedFiles) {
+                    execCommand(`git add ${file}`)
+                }
+                execCommand(`git commit -m "chore: ${commitMsg}"`)
+            }
+        }
+    } catch (error) {
+        console.error('Error checking git status')
+        process.exit(1)
+    }
+}
+async function updateChangelog(packageName) {
+    const basePath = __dirname.split('sfra-release')
+    const packagePath = path.resolve(basePath[0], packageName)
+    const pkg = path.resolve(packagePath, 'package.json')
+    const pkgContent = JSON.parse(fs.readFileSync(pkg, 'utf8'))
+    const date = new Date().toString().split(' ').slice(1, 4)
+    const heading = `## v${pkgContent.version} (${date[0]} ${date[1]}, ${date[2]})`
+
+    const changelogPath = path.resolve(packagePath, 'CHANGELOG.md')
+    let currentChangeLog = fs.readFileSync(changelogPath, 'utf8')
+    const title = /# Changelog/
+    const tempChangeLog = path.resolve(os.tmpdir(), 'CHANGELOG.md')
+    if (currentChangeLog.match(title)) {
+        currentChangeLog = currentChangeLog.replace(title, '')
+        fs.writeFileSync(tempChangeLog, '# Changelog\n\n', 'utf8')
+        fs.appendFileSync(tempChangeLog, heading, 'utf8')
+        fs.appendFileSync(tempChangeLog, currentChangeLog, 'utf8')
+        fs.copyFileSync(tempChangeLog, changelogPath)
+        console.log('Updated Changelog.')
+    }
+}
+async function updateStorefrontData(packageName, VERSION) {
+    const versionWithoutV = VERSION.replace(/^v/, '')
+
+    console.log('\x1b[33m>Updating xml file in StorefrontData package\x1b[0m')
+    const xmlFile = path.resolve('demo_data_sfra/libraries/RefArchSharedLibrary/library.xml')
+    if (fs.existsSync(xmlFile)) {
+        let content = fs.readFileSync(xmlFile, 'utf8')
+        const regex = /&lt;!-- SFRA 7.0.0/g
+        const newLine = `&lt;!-- SFRA ${versionWithoutV}`
+        if (content.match(regex)) {
+            content = content.replace(regex, newLine)
+            fs.writeFileSync(xmlFile, content, 'utf8')
+            console.log(`Updated ${xmlFile} with new version ${versionWithoutV}`)
+        }
+    }
+}
+async function preparePRsToRelease(VERSION, MAIN_BRANCH, packages = defaultPackages) {
+    console.log('\x1b[36m%s\x1b[0m', '======================= PR Creation Step =================')
+    console.log(`\nVersion: ${VERSION}`)
+    console.log(`Main Branch: ${MAIN_BRANCH}`)
+    console.log(`Packages: ${packages.join(', ')}\n`)
+    const createdPRs = []
+    for (const packageName of packages) {
+        console.log(
+            '\x1b[36m%s\x1b[0m',
+            `======================= Processing package: ${packageName} =================`
+        )
+
+        const basePath = __dirname.split('sfra-release')
+        const packagePath = path.resolve(basePath[0], packageName)
+
+        if (!fs.existsSync(packagePath)) {
+            console.error(`Package directory ${packageName} does not exist.`)
+            continue
+        }
+
+        process.chdir(packagePath)
+
+        console.log('\x1b[33m> Switching to main/master branch \x1b[0m')
+        const branchToUse =
+            packageName === 'storefront-reference-architecture'
+                ? 'integration'
+                : packageName === 'plugin-slas'
+                  ? 'main'
+                  : MAIN_BRANCH
+
+        await checkForChanges()
+        execCommand(`git switch ${branchToUse}`)
+        execCommand('git pull')
+
+        await checkForChanges()
+
+        console.log('\x1b[33m> Switching to release branch \x1b[0m')
+        const branchName = `release/${VERSION}`
+        let branchExists = false
+
+        try {
+            execSync(`git rev-parse --verify ${branchName}`)
+            branchExists = true
+        } catch (err) {
+            branchExists = false
+        }
+        if (branchExists) {
+            execCommand(`git switch ${branchName}`)
+        } else {
+            execCommand(`git switch -c ${branchName}`)
+        }
+
+        console.log('\x1b[33m> Running npm install')
+        execCommand('npm install')
+        await checkForChanges()
+
+        updatePropertiesFile(packageName, VERSION)
+        if (packageName === 'storefrontdata') {
+            await updateStorefrontData(packageName, VERSION)
+        }
+        updatePackageJsonVersion(packageName, VERSION)
+        await checkForChanges(false, `chore: release ${VERSION}`)
+        console.log(
+            `\x1b[33m> Pushing the change to remote branch: git push -u origin ${branchName}\x1b[0m`
+        )
+        execCommand(`git push -u origin ${branchName}`)
+
+        const prURL = await createPR(packageName, branchToUse, VERSION)
+        if (prURL) {
+            createdPRs.push(prURL)
+        }
+
+        await updateChangelog()
+
+        process.chdir('..')
+    }
+    createdPRs.forEach((prURL) => {
+        console.log(prURL)
+    })
+}
+
+async function createGitTag(VERSION, MAIN_BRANCH, packagesToProcess) {
+    for (const packageName of packagesToProcess) {
+        console.log(
+            '\x1b[36m%s\x1b[0m',
+            `======================= Processing package: ${packageName} =================`
+        )
+
+        console.log('\x1b[36m%s\x1b[0m', `Creating git tag`)
+
+        const basePath = __dirname.split('sfra-release')
+        const packagePath = path.resolve(basePath[0], packageName)
+
+        if (!fs.existsSync(packagePath)) {
+            console.error(`Package directory ${packageName} does not exist.`)
+            continue
+        }
+
+        process.chdir(packagePath)
+        const branchToUse =
+            packageName === 'storefront-reference-architecture'
+                ? 'integration'
+                : packageName === 'plugin-slas'
+                  ? 'main'
+                  : MAIN_BRANCH
+        console.log('branchToUse', branchToUse)
+        execCommand(`git switch ${branchToUse}`)
+        execCommand('git pull')
+        // Check if the tag already exists
+        const existingTags = execSync('git tag').toString().split('\n')
+        if (existingTags.includes(VERSION)) {
+            console.log(`Tag ${VERSION} already exists. Skipping tag creation.`)
+        } else {
+            execCommand(`git tag ${VERSION}`)
+        }
+
+        execCommand(`git push origin refs/tags/${VERSION}`)
+    }
+}
+
+
+async function main() {
+    const args = process.argv.slice(2)
+    if (args.length < 3) {
+        console.error(
+            'Usage: node prep-PR.js.js <VERSION> <MAIN_BRANCH> <action> [<package1> <package2> ... <packageN>]'
+        )
+        console.error(
+            'Example for PR creation with specific packages: node prep-PR.js v1.0.0 main createPR lib_productlist plugin-applepay'
+        )
+        console.error(
+            'Example for PR creation with default packages: node prep-PR.js v1.0.0 main createPR'
+        )
+        console.error(
+            'Example for Git tag creation with specific packages: node prep-PR.js v1.0.0 main createGitTag lib_productlist plugin-applepay'
+        )
+        console.error(
+            'Example for Git tag creation with default packages: node prep-PR.js v1.0.0 main createGitTag'
+        )
+        process.exit(1)
+    }
+
+    const [VERSION, MAIN_BRANCH, action, ...packages] = args
+
+    let packagesToProcess = packages.length > 0 ? packages : defaultPackages
+
+    switch (action) {
+        case 'createPR':
+            await preparePRsToRelease(VERSION, MAIN_BRANCH, packagesToProcess)
+            break
+        case 'createGitTag':
+            await createGitTag(VERSION, MAIN_BRANCH, packagesToProcess)
+            break
+        default:
+            console.error('Invalid action. Please specify either "createPR" or "createGitTag".')
+            process.exit(1)
+    }
+
+    rl.close()
+}
+
+main()
